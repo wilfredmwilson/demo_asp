7daad22192ea3add3cf9487f27fa99afda7d2f66
wilfredmwilson <fredywilsonprince@gmail.com>
2023-11-29 13:35:03 +0530

    Add project files.

---
+++ Demo/Controllers/StudentController.cs
@@ -0,0 +1,69 @@
+﻿using Demo.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        db_testEntities db = new db_testEntities();
+        public ActionResult Student(tbl_Student obj)
+        {
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult AddStudent(tbl_Student model)
+        {
+            tbl_Student obj = new tbl_Student();
+            if (ModelState.IsValid)
+            {
+                obj.ID = model.ID;
+                obj.Name = model.Name;
+                obj.Fname = model.Fname;
+                obj.Email = model.Email;
+                obj.Mobile = model.Mobile;
+                obj.Description = model.Description;
+
+                if (model.ID > 0)
+                {
+                    db.Entry(obj).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.tbl_Student.Add(obj);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+            }
+
+
+            return RedirectToAction("StudentList");
+        }
+
+
+        public ActionResult StudentList()
+        {
+            var res = db.tbl_Student.ToList();
+            return View(res);
+        }
+
+       public ActionResult Delete(int id)
+        {
+            var res = db.tbl_Student.Where(x => x.ID == id).First();
+            db.tbl_Student.Remove(res);
+            db.SaveChanges();
+
+            var list = db.tbl_Student.ToList();
+
+            return View("StudentList", list);
+        }
+       
+
+    }
+}
---
+++ Demo/Controllers/db_TestingEntities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Demo.Context;
+
+namespace Demo.Controllers
+{
+    internal class db_TestingEntities
+    {
+        internal object tbl_Student;
+
+        internal object Entry(tbl_Student obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
